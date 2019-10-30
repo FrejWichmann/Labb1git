e994c01070bb16230efa9c0e80f21cb83d7b1daf
Frej Wichmann <frejwichmann@gmail.com>
2019-10-30 10:21:43 +0100

    new main

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,6 +3,10 @@ package sample;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int i = (int) Math.random();
+        if (i>100) {
+            System.out.println("Tja");
+        }
+        else System.out.println("Tjena");
     }
 }
